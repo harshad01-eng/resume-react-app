6d4214e57aa6de56b9566b3dc5584c8ee88bbd54
harshad01-eng <80380519+harshad01-eng@users.noreply.github.com>
2022-03-28 13:36:30 +0530

    avtar image setting

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header1 from "./Components/header1";
 // import imgBg from './Assets/Images/programBg.jpg';
+import Avatar from '@mui/material/Avatar';
 export default class home extends React.Component{
     constructor(props){
     super(props);this.setState={
@@ -14,7 +15,17 @@ return(
     <div>
     <div className="App backgraoundImg">
             <Header1/>
-
+            <div style={{display:'flex', justifyContent:'center', alignContent:'center', marginTop:'8%',}}>
+                
+                <Avatar sx={{ width: 150, height:150}} alt="Remy Sharp" src={require("./Assets/Images/profile.jpg")} />
+              
+                   
+          
+            </div>
+            <div  style={{display:'flex', justifyContent:'center', alignContent:'center', color:'white'}}>
+                <p style={{fontSize:34, fontWeight:'bold', marginTop:'auto'}}>Software Developer</p>
+            </div>
+            
     </div>   
     
     </div>
